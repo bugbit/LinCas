a29f95baa5032ef9968bbd77d069dedc3a0b1699
bugbit <ohernandezba@gmail.com>
2018-05-29 15:38:39 +0200

    test isprime

--- Tests1/TestFactorialDecomposition.cs
+++ Tests1/TestFactorialDecomposition.cs
@@ -8,8 +8,56 @@ namespace Tests1
 {
     class TestFactorialDecomposition
     {
+        static bool IsPrime(int n)
+        {
+            if (n == 1 || n == 2)
+                return true;
+            
+            if ((n % 2) == 0)
+                return false;
+
+            for (int i = 3; i < n; i += 2)
+                if ((n % i) == 0)
+                    return false;
+
+            return true;
+        }
+        static bool IsPrime(long n)
+        {
+            if (n == 1 || n == 2)
+                return true;
+
+            if ((n % 2) == 0)
+                return false;
+
+            for (int i = 3; i < n; i += 2)
+                if ((n % i) == 0)
+                    return false;
+
+            return true;
+        }
+        static bool IsPrimed(dynamic n)
+        {
+            if (n == 1 || n == 2)
+                return true;
+
+            if ((n % 2) == 0)
+                return false;
+
+            for (int i = 3; i < n; i += 2)
+                if ((n % i) == 0)
+                    return false;
+
+            return true;
+        }
         static void Main(string[] args)
         {
+            int n = 11;
+            var p1 = IsPrime(n);
+            var pd1 = IsPrimed(n);
+            long l = 11;
+            var p2 = IsPrime(l);
+            var pd2 = IsPrimed(l);
         }
     }
 }
