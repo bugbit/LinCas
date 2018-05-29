96b3da09e0a740431359443bc5fc69bd3c876ec6
bugbit <ohernandezba@gmail.com>
2018-05-29 23:55:55 +0200

    isprime

--- Tests1/TestFactorialDecomposition.cs
+++ Tests1/TestFactorialDecomposition.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,26 @@ namespace Tests1
 {
     class TestFactorialDecomposition
     {
+        //private static readonly Expression<Func<int, bool>> mIsPrimeExpr = n =>
+        //   {
+        //       if (n == 1 || n == 2)
+        //           return true;
+
+        //       if ((n % 2) == 0)
+        //           return false;
+
+        //       for (int i = 3; i < n; i += 2)
+        //           if ((n % i) == 0)
+        //               return false;
+
+        //       return true;
+        //   };
+
         static bool IsPrime(int n)
         {
             if (n == 1 || n == 2)
                 return true;
-            
+
             if ((n % 2) == 0)
                 return false;
 
@@ -52,6 +68,7 @@ namespace Tests1
         }
         static void Main(string[] args)
         {
+            //Expression<Func<int, bool>> pExpr1 = mIsPrimeExpr;
             int n = 11;
             var p1 = IsPrime(n);
             var pd1 = IsPrimed(n);
