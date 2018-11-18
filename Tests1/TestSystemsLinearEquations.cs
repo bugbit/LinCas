57b21ea5f0988a54e40b7863425bf0544ce37aa3
bugbit <ohernandezba@gmail.com>
2018-11-18 17:12:05 +0100

    Update Tests1/TestSystemsLinearEquations.cs

--- Tests1/TestSystemsLinearEquations.cs
+++ Tests1/TestSystemsLinearEquations.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,27 @@ namespace Tests1
 {
     class TestSystemsLinearEquations
     {
+        static void linsolve<T>(Expression<T> argExpr)
+        {
+
+        }
+        static void linsolve(LambdaExpression argExpr)
+        {
+
+        }
+        static void linsolve(Expression<Func<decimal, bool>> argExpr)
+        {
+
+        }
         static void Main(string[] args)
         {
+            //linsolve(x => x == 1);
+
+            Expression<Func<decimal, bool>> e = x => x == 1;
+
+            linsolve(e);
+            linsolve<Func<decimal, bool>>(x => x == 1);
+
             Func<decimal, decimal, decimal, bool> pList1 =
                 (x, y, z) => x + y + z == 20m;
             Func<decimal, decimal, decimal, bool[]> pList2 =
